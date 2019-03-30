c0c7e4bfae3f66e43a2878955bb2656d3e6f6863
Jack O'Sullivan <jackos1998@gmail.com>
2019-03-30 12:20:23 +0000

    Assignment 5: Initial commit

---
+++ assignment5/src/test/java/CompetitionTests.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class CompetitionTests {
+    @Test
+    public void testDijkstraConstructor() {
+        // TODO: implement
+    }
+
+    @Test
+    public void testFWConstructor() {
+        // TODO: implement
+    }
+
+    // TODO: add more tests
+}
